1ee4073ceb307c678259659ccab05c29333d6d55
Atte Silen <atte.silen@haltu.fi>
2020-04-06 18:25:15 +0300

    Id int to str

--- respa_payments/integrations/paytrail_e2_utils.py
+++ respa_payments/integrations/paytrail_e2_utils.py
@@ -2,7 +2,7 @@ import unicodedata
 
 
 def generate_order_number(service, resource_name, order_id):
-    return service + '+' + unicode_to_paytrail(resource_name) + '+' + order_id,
+    return service + '+' + unicode_to_paytrail(resource_name) + '+' + str(order_id),
 
 
 def unicode_to_paytrail(string):
